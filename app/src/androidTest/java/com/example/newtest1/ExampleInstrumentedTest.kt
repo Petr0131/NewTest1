e72843bd05c5ba4fb5b73ae517dd0d13b643d612
“Petr <nip2001@mail.ru>
2024-03-12 00:57:36 +0600

    test test test)

--- app/src/androidTest/java/com/example/newtest1/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/newtest1/ExampleInstrumentedTest.kt
@@ -20,7 +20,7 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         // test test test
         // test test test
-        // test test test
+        // testov Net
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.newtest1", appContext.packageName)
     }
